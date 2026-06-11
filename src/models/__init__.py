e9f935c238614a386da714187cc1c2fbea971b79
31grudnia <mikolaj.starczewski@gmail.com>
2026-06-12 00:51:31 +0200

    implemented Facade for models

---
+++ src/models/__init__.py
@@ -0,0 +1,13 @@
+from src.models.point import Point
+from src.models.panel import Panel
+from src.models.segment import Segment
+from src.models.mount import Mount
+from src.models.joint import Joint
+
+__all__ = [
+    "Point",
+    "Panel",
+    "Segment",
+    "Mount",
+    "Joint",
+]
