baf6d8a73eb90218848095db3cf3b0bc19e0e418
31grudnia <mikolaj.starczewski@gmail.com>
2026-06-12 00:43:15 +0200

    implemented serialization to point class

--- src/models/point.py
+++ src/models/point.py
@@ -6,5 +6,16 @@ class Point:
     x: float
     y: float
 
+    def to_dict(self, precision: int | None = None) -> dict[str, float]:
+        if precision is not None:
+            return {
+                "x": round(self.x, precision),
+                "y": round(self.y, precision)
+            }
+        return {
+            "x": self.x,
+            "y": self.y
+        }
+
     def __repr__(self) -> str:
         return f"Point(x={self.x}, y={self.y})"
