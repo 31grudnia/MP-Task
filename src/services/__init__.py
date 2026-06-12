0ae13977e2cc9f0642ad33c69ad10fd651467e1b
31grudnia <mikolaj.starczewski@gmail.com>
2026-06-12 02:26:09 +0200

    implementation init.py file for services

---
+++ src/services/__init__.py
@@ -0,0 +1,15 @@
+from src.services.interfaces import (
+    MountCalculatorInterface,
+    JointCalculatorInterface,
+)
+from src.services.mount_calculator import MountCalculator
+from src.services.joint_calculator import JointCalculator
+from src.services.solar_array_service import SolarArrayService
+
+__all__ = [
+    "MountCalculatorInterface",
+    "JointCalculatorInterface",
+    "MountCalculator",
+    "JointCalculator",
+    "SolarArrayService",
+]
